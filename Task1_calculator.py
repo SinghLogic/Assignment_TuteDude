4ed413e362448a8d12b604e10e080c064aa99441
SinghLogic <aniketvijaysingh123@gmail.com>
2025-07-14 11:09:33 +0530

    Add files via upload

---
+++ Task1_calculator.py
@@ -0,0 +1,17 @@
+#Perform Basic Mathematical Operations
+
+#Get input from the user
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+
+#basic mathematical operations
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+
+#Display the results
+print(f"\nAddition = {addition}")
+print(f"Subtraction = {subtraction}")
+print(f"Multiplication = {multiplication}")
+print(f"Division = {division}")
---
+++ Task2_greeting.py
@@ -0,0 +1,23 @@
+#Create a Personalized Greeting
+
+'''
+#Type 1
+x=(input("Enter your first name:"))
+y=(input("Enter your last name:"))
+
+print(f"\nHello, {x} {y}! Welcome to the Python program")
+'''
+
+#Type2
+#Get user's first name and last name
+
+First_Name = (input("Enter your first name:"))
+Last_Name =  (input("Enter your last name:"))
+
+#Concatenates the first name and last name into a full name
+
+full_name = First_Name + " " + Last_Name
+
+#Prints a personalized greeting
+
+print(f"\nHello, {full_name}! Welcome to the python program")
