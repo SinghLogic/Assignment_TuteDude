4a0d1aa6d50f90be4bdd9341348ed7f5f5366696
SinghLogic <aniketvijaysingh123@gmail.com>
2025-07-14 12:20:41 +0530

    Add files via upload

---
+++ Assign_1 to 50 Num.py
@@ -0,0 +1,7 @@
+
+total= 0
+
+for a in range(1,51): # Numbers from 1 to 50
+    total += a
+
+print("The sum of number from 1 to 50 is:", total)
---
+++ Assign_Even or Odd.py
@@ -0,0 +1,10 @@
+#Integer input from the user.
+
+x=int(input("Enter the number:"))
+
+#Checks if number is Even or Odd.
+if x % 2 == 0:
+ print(f"{x} is even.")
+
+else:
+ print(f"{x} is odd.")
